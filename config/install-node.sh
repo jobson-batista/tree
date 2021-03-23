a059064b9b1ed98e74ea947dde68f813dde93f5c
Jobson Batista <jobson.silva@dcx.ufpb.br>
2021-03-23 23:43:20 +0000

    Script de instalacao do NodeJS

--- config/install-node.sh
+++ config/install-node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 clear
+
 #Identificando Distro
 so=`hostnamectl | grep 'Operating System' | cut -d: -f2`
 
@@ -9,7 +9,8 @@ user=`who -u | cut -d" " -f1`
 
 #Identificando caminho atual
 pathRole=`pwd`
-dicName=node-v14.16.0
+dicN=node-v14.16.0
+NODEJS=node-v14.16.0-linux-x64
 
 echo "+------------------------------+"
 echo "| Configurando Ambiente NodeJS |"
@@ -17,27 +18,31 @@ echo "|              by Tree         |"
 echo "+------------------------------+"
 echo ""
 
-echo "Distro:  $so"
-echo "User:     $user"
+echo "Distro:  		$so"
+echo "Nome de usuário:	$user"
 echo ""
 
 echo "--> Baixando o NodeJS v14.16.0 ..."
 echo ""
-wget -c -q --show-progress https://nodejs.org/dist/v14.16.0/node-v14.16.0-linux-x64.tar.xz -O $dicName.tar.xz
+wget -c -q --show-progress https://nodejs.org/dist/v14.16.0/node-v14.16.0-linux-x64.tar.xz
 
 echo ""
 echo "--> Descompactando..."
 echo ""
-tar -xf $dicName.tar.xz
+tar -xf $NODEJS.tar.xz
 
 echo ""
 echo "Pronto! Descompactado!"
 echo ""
 
-echo "Movendo para $HOME/.local/opt"
-# mv $dicName/ $HOME/.local/opt
+echo "Movendo para /opt"
+sudo cp -r $NODEJS/ /opt
 
-# rm -r node*
+# Remover arquivos baixados
+rm -r node*
 
+echo "export PATH=/opt/$NODEJS/bin:$PATH" >> ~/.profile
+. ~/.profile
 
-
+echo "Pronto!"
+echo "Node instalado com sucesso!"
