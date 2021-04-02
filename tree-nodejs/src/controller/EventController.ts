134ad790b56d6827bf81bb466cd6ef3ecf6df94f
Jobson Batista <jobson.silva@dcx.ufpb.br>
2021-04-02 13:23:25 -0300

    #28 - Removendo comentario

--- tree-nodejs/src/controller/EventController.ts
+++ tree-nodejs/src/controller/EventController.ts
@@ -45,7 +45,5 @@ export const deleteEvent = async (req: Request, res: Response) => {
     const event = await getRepository(Event).findOne(id);
     await getRepository(Address).delete(event.address.id);
     await getRepository(Event).delete(id);
-    
-    //const addressId = await getRepository(Event)
     return res.status(200).json({ message: "Event deleted" });
 }
