053575ee5702f5fb98bcddfa256af2a4efcdffdc
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-04-09 19:49:28 -0300

    Import do component "footer" atualizado

--- tree-angular/src/app/app.module.ts
+++ tree-angular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { CardComponent } from './components/card/card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
