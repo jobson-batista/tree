fa300d052508a210e7607b0a5d0d7ed3c94b83b4
Ismar Rodrigues <ismar.rodrigues@dcx.ufpb.br>
2021-04-28 20:34:57 -0300

    Adicionado declaracao

--- tree-angular/src/app/app.module.ts
+++ tree-angular/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VacancyCardComponent } from './vacancy-card/vacancy-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VacancyCard
+    VacancyCardComponent
   ],
   imports: [
     BrowserModule,
