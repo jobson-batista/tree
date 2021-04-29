db342ebd8fc4e698cb5300489f302689caf15b00
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-04-29 10:34:06 -0300

    - Componente VacancyCardComponent adicionado

--- tree-angular/src/app/app.module.ts
+++ tree-angular/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { VacancyCardComponent } from './components/vacancy-card/vacancy-card.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,7 +7,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VacancyCardComponent
   ],
   imports: [
     BrowserModule,
