c7199dcba36040c7c9428b1204aaa6abf4b635fb
joanasoaresd <joana.darck@dcx.ufpb.br>
2021-05-01 22:45:59 -0300

    Issue #42 - Adicionando telinha de cadastro, falta responsividade

--- tree-angular/src/app/app.module.ts
+++ tree-angular/src/app/app.module.ts
@@ -7,14 +7,16 @@ import { UserCardComponent } from './components/userCard/userCard.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserCardComponent
+    UserCardComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
