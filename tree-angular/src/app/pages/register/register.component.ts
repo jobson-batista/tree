2f03c060b6f6f8798f8d5931c3ca98f075f8cba3
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-05-02 16:03:02 -0300

    refs #42 - Responsividade adicionada

--- tree-angular/src/app/pages/register/register.component.ts
+++ tree-angular/src/app/pages/register/register.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-register',
+  selector: 'page-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
+  @Input() isLogin: Boolean = false;
+  textTop: String = 'Cadastrar-se';
+  textBottom: String = 'Já possui uma conta?'
+  textOption: String = 'Entrar';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setOption(value: Boolean):void {
+    this.isLogin = value;
+    this.textOption = this.textTop;
+    this.textTop = value ? 'Entrar': 'Cadastrar-se';
+    this.textBottom = value ? 'Ainda não possui uma conta?' : 'Já possui uma conta?';
   }
 
 }
