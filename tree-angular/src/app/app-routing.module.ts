c5a7dc73022c0c9a8abb880370614254c6096ce9
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-05-16 20:54:17 -0300

    Adicionado rota para login/cadastro de usuário

--- tree-angular/src/app/app-routing.module.ts
+++ tree-angular/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RegisterComponent } from './pages/register/register.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommunityComponent } from './pages/community/community.component';
@@ -5,7 +6,8 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'community', component: CommunityComponent }
+  { path: 'community', component: CommunityComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
