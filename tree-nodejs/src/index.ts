5f082cb0b3f783f9d5b961acda616a3e5710a411
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-07-15 13:10:23 -0300

    Atualização da porta 4200 para 8081

--- tree-nodejs/src/index.ts
+++ tree-nodejs/src/index.ts
@@ -10,7 +10,7 @@ import * as cors from 'cors';
 
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:4200',
+    origin: 'http://localhost:8081',
     optionsSuccessStatus: 200
 }
 
