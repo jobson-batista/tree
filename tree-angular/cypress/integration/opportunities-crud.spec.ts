11bb657327222084a04fcab50f2b55e2ddc4e9b7
joanasoaresd <joana.darck@dcx.ufpb.br>
2021-07-16 04:27:19 -0300

    Adicionando e2e de crud de oportunidades

--- tree-angular/cypress/integration/opportunities-crud.spec.ts
+++ tree-angular/cypress/integration/opportunities-crud.spec.ts
@@ -27,8 +27,25 @@ describe('Fluxo de cadastro de oportunidades', () => {
         cy.get('#dateId').click()
         cy.contains('15').click()
         cy.get('[data-test=submit]').first().click();
-        cy.intercept({method: 'POST', url: 'http://localhost:3333/api/jobs'}).as('registerJob');
+        cy.intercept({ method: 'POST', url: 'http://localhost:3333/api/jobs' }).as('registerJob');
         cy.wait('@registerJob');
+    });
+
+    it('Pesquisar e Apagar Oportunidades - Job', () => {
+        cy.visit('/');
+        cy.get('[data-test=goOportunities]').first().click();
+        cy.contains('Encontre');
+        cy.contains('oportunidades');
+        cy.contains('facilmente!');
+        cy.url().should('contain', '/opportunities')
+        cy.intercept({ method: 'GET', url: 'http://localhost:3333/api/jobs' }).as('getJob');
+        cy.get('[data-test=searchTest]').first().click()
+        cy.contains('Estágio/Emprego')
+        cy.get('[data-test=getJobTest]').first().click()
+        cy.get('[data-test=dellTest]').first().click()
+        cy.contains('Remover')
+        cy.get('[data-test=confirmDellTest]').first().click()
+        cy.intercept({ method: 'DELETE', url: 'http://localhost:3333/api/jobs' }).as('deleteJob');
     });
 
     it('Cadastro de Oportunidades - Event', () => {
@@ -55,8 +72,25 @@ describe('Fluxo de cadastro de oportunidades', () => {
         cy.get('#endDateId').click()
         cy.contains('25').click()
         cy.get('[data-test=submit]').first().click();
-        cy.intercept({method: 'POST', url: 'http://localhost:3333/api/events'}).as('registerEvents');
+        cy.intercept({ method: 'POST', url: 'http://localhost:3333/api/events' }).as('registerEvents');
         cy.wait('@registerEvents');
+    });
+
+    it('Pesquisar e Apagar Oportunidades - Event', () => {
+        cy.visit('/');
+        cy.get('[data-test=goOportunities]').first().click();
+        cy.contains('Encontre');
+        cy.contains('oportunidades');
+        cy.contains('facilmente!');
+        cy.url().should('contain', '/opportunities')
+        cy.intercept({ method: 'GET', url: 'http://localhost:3333/api/events' }).as('getEvents');
+        cy.get('[data-test=searchTest]').first().click()
+        cy.contains('Eventos')
+        cy.get('[data-test=getEventTest]').first().click()
+        cy.get('[data-test=dellTest]').first().click()
+        cy.contains('Remover')
+        cy.get('[data-test=confirmDellTest]').first().click()
+        cy.intercept({ method: 'DELETE', url: 'http://localhost:3333/api/events' }).as('deleteEvents');
     });
 
     it('Cadastro de Oportunidades - Specialization', () => {
@@ -82,10 +116,29 @@ describe('Fluxo de cadastro de oportunidades', () => {
         cy.contains('Descrição')
         cy.get('#desc').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque congue sapien in tincidunt ornare. Suspendisse potenti. Nunc volutpat consectetur urna in congue. Praesent ac consectetur nibh. Ut sed lacinia nisi, sed porttitor nunc. Quisque in leo eu dolor tincidunt malesuada. Nunc lacinia accumsan dui non feugiat.')
         cy.get('[data-test=submit]').first().click();
-        cy.intercept({method: 'POST', url: 'http://localhost:3333/api/specializations'}).as('registerSpecializations');
+        cy.intercept({ method: 'POST', url: 'http://localhost:3333/api/specializations' }).as('registerSpecializations');
         cy.wait('@registerSpecializations');
     });
 
-
+    it('Pesquisar e Apagar Oportunidades - Specialization', () => {
+        cy.visit('/');
+        cy.get('[data-test=goOportunities]').first().click();
+        cy.contains('Encontre');
+        cy.contains('oportunidades');
+        cy.contains('facilmente!');
+        cy.url().should('contain', '/opportunities')
+        cy.intercept({ method: 'GET', url: 'http://localhost:3333/api/specializations' }).as('getSpecializations');
+        cy.get('[data-test=searchTest]').first().click()
+        cy.contains('Especialização')
+        cy.get('[data-test=getSpecTest]').first().click()
+        cy.get('[data-test=dellTest]').first().click()
+        cy.contains('Cancelar')
+        cy.get('[data-test=cancellDellTest]').first().click()
+        cy.intercept({ method: 'GET', url: 'http://localhost:3333/api/specializations' }).as('getSpecializations');
+        cy.get('[data-test=dellTest]').first().click()
+        cy.contains('Remover')
+        cy.get('[data-test=confirmDellTest]').first().click()
+        cy.intercept({ method: 'DELETE', url: 'http://localhost:3333/api/specializations' }).as('deleteSpecializations');
+    });
 });
 
